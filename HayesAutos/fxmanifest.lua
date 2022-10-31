7d782ffc9864705893979d7281a43a47e3171c1b
McNastty6699 <88422603+McNastty6699@users.noreply.github.com>
2022-10-31 17:38:51 -0500

    Add files via upload

---
+++ HayesAutos/fxmanifest.lua
@@ -0,0 +1,12 @@
+name "HayesAutos"
+author "McNastty6699"
+description "Custom HayesAutos V1.0"
+
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+server_scripts {
+	'server.lua',
+}
---
+++ HayesAutos/server.lua
@@ -0,0 +1,3 @@
+print('\n\n- - - - - - - - - - - - - - - - - - - - - -\n^7[^HAYESAUTO^7] ^7[^CUSTOM MAPP CREATED BY MCNASTTY#1922^7] ^HAYESAUTO.^0\n- - - - - - - - - - - - - - - - - - - - - -\n')
+           
+      
