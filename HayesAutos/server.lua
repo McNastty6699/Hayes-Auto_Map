6ad80c68a96e2132e3a651ca1a132c92893c2fc9
McNastty6699 <88422603+McNastty6699@users.noreply.github.com>
2022-10-31 19:49:31 -0500

    Delete HayesAutos directory

--- HayesAutos/server.lua
+++
@@ -1,3 +0,0 @@
-print('\n\n- - - - - - - - - - - - - - - - - - - - - -\n^7[^HAYESAUTO^7] ^7[^CUSTOM MAPP CREATED BY MCNASTTY#1922^7] ^HAYESAUTO.^0\n- - - - - - - - - - - - - - - - - - - - - -\n')
-           
-      
